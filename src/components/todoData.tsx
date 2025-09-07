47cc677cf42a56251f1f8a436e42bd453f1feeac
Lê Đăng Khoa <khoald.24it@vku.udn.vn>
2025-09-07 11:19:17 +0700

    Update code lần 3

--- src/components/todoData.tsx
+++ src/components/todoData.tsx
@@ -1,3 +1,4 @@
+// interface sẽ dùng để định nghĩa Object (nên code OOP)
 interface IProps {
   todo: {
     id: number;
@@ -6,6 +7,7 @@ interface IProps {
   }[];
 }
 
+// type
 type TProps = {
   todo: {
     id: number;
@@ -13,6 +15,27 @@ type TProps = {
     isComplete: boolean;
   }[];
 };
+// ======================================================================================
+// Lưu ý :
+// Nên code interface tới khi mà code type
+// Nên dùng Interface khi code Hướng Đối Tượng (OOP)
+// Nên dùng Type khi dùng thuộc tính đơn giản của một đối tượng
+//
+// Ex :
+// Khá bất tiện khi dùng interface chỉ để định nghĩa một thuộc tính đơn giản của Object
+// colors: "red" | "yellow" | "pink"
+//
+// interface IColor {
+//   colors: "red" | "yellow" | "pink"
+// }
+//
+// Sử dụng type sẽ hợp lí hơn và nó sẽ gợi ý code
+//
+// type TColor = "red" | "yellow" | "pink"
+//
+// const color: TColor = 'pink';
+// ======================================================================================
+
 // props: IProps or TProps
 const TodoData = (props: TProps) => {
   const { todo } = props;
